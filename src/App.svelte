3e4a248209cd25d6e1334d5e49f3f8e0b7fe5e5d
Alexander Katrukhin <olkatruk@microsoft.com>
2022-08-31 11:47:33 -0700

    Main banner

--- src/App.svelte
+++ src/App.svelte
@@ -1,55 +1,59 @@
 <script lang="ts">
   import { Router, Route } from "svelte-routing";
+  import { fade } from 'svelte/transition';
   import Loading from './lib/Loading.svelte';
   import Header from './lib/Header.svelte';
   import LazyComponent from "./lib/Loading copy.svelte";
   import { routes } from "./routes/utils"
-
+  
   let main;
   let scrollY = 0;
-
+  
   const LOADING_MOUNT_DELAY = 1000;
   const LOADING_ANIMATION_DURATION = 2000;
   const LOADING_ANIMATION_SEQUENCE_DELAY = 300;
-
+  
   let didLoadingComponentMount = false;
   let isAnimationLoadingPlayed = false;
-
+  
   setTimeout(() => {
     didLoadingComponentMount = true
   }, LOADING_MOUNT_DELAY);
-
+  
   setTimeout(() => {
     isAnimationLoadingPlayed = true
-  }, LOADING_MOUNT_DELAY + LOADING_ANIMATION_DURATION + LOADING_ANIMATION_SEQUENCE_DELAY * 3);
+  }, LOADING_MOUNT_DELAY + LOADING_ANIMATION_DURATION);
 </script>
 
-<main class="h-screen w-screen flex flex-col text-base">
+<div class="h-screen w-screen flex flex-col text-base selection:bg-teal-400 dark:selection:bg-teal-500">
+  {#if didLoadingComponentMount}
   <div 
-    class="flex flex-col w-full h-full overflow-x-hidden overflow-scroll transition-opacity duration-100 ease-out bg-zinc-100 dark:bg-zinc-800" class:opacity-0={!didLoadingComponentMount} 
+    class="flex flex-col h-screen w-screen overflow-x-hidden overflow-scroll bg-zinc-100 dark:bg-zinc-800" class:opacity-0={!didLoadingComponentMount} 
     bind:this={main} 
     on:scroll={
-      () => {
-        scrollY = main.scrollTop
-      }} 
-  >
+    () => {
+      scrollY = main.scrollTop
+    }} 
+    transition:fade
+    >
     <Router>
       <Header scrollY={scrollY} />
-
-      {#each routes as page}      
+      <main class="max-w-[1560px] mx-auto w-full px-[9vw]">
+        {#each routes as page}     
         <Route path={page.route}>
           <LazyComponent 
-	          when={page.route === window.location.pathname}
-	          component={() => import(`./routes/${page.name}.svelte`)} 
+          when={page.route === window.location.pathname}
+          component={() => import(`./routes/${page.name}.svelte`)} 
           />
         </Route>
-      {/each}
-
+        {/each}
+      </main>
     </Router>
   </div>
+  {/if}
   {#if !isAnimationLoadingPlayed}
     <Loading startDelay={LOADING_MOUNT_DELAY} duration={LOADING_ANIMATION_DURATION} sequenceDelay={LOADING_ANIMATION_SEQUENCE_DELAY} />
   {/if}
-</main>
+</div>
 
 <svelte:window bind:scrollY={scrollY} />
--- src/routes/About.svelte
+++ src/routes/About.svelte
@@ -1,51 +1,51 @@
 <script>
-  import { onMount } from 'svelte';
-  const cdn = 'https://cdn.jsdelivr.net/npm/typed.js@2.0.12';
-  
-  const options = {
-    strings: ['<i>First</i> sentence.', '&amp; a second sentence.'],
-    typeSpeed: 40
+  let underline;
+  function typewriter(node, { speed = 1 }) {
+    const valid = (
+    node.childNodes.length === 1 &&
+    node.childNodes[0].nodeType === Node.TEXT_NODE
+    );
+    
+    if (!valid) {
+      throw new Error(`This transition only works on elements with a single text node child`);
+    }
+    
+    const text = node.textContent;
+    const duration = text.length / (speed * 0.01);
+    
+    return {
+      duration,
+      tick: t => {
+        const i = Math.trunc(text.length * t);
+        node.textContent = text.slice(0, i);
+      }
+    };
   }
-
-  // let lazyComponent;
-	// function load() {
-	// 	lazyComponent = import(cdn);
-	// }
-
-  // console.log()
-  
-  onMount(() => {
-    // import(cdn) // .js can be skipped
-    // .then(module => {
-    //     // stop loading indicator
-    //     const Typed = module.default;
-    //     new Typed('#typed-string', options)
-    // })
-    // .then((x) => console.log(x))
-    // .catch((err) => {
-    //   console.log({err})
-    // });
-
-    // await lazyComponent;
-    // console.log(lazyComponent)
-  })
 </script>
 
-<h1> Hello
-  <span id="typed-string"></span>
-</h1>
+<section class="relative mt-[12vh] mb-[16vh]">
+  <h1 class="cpd text-5xl pt-[15vh]">
+    <span class="flex items-center">
+      Hi, I'm an UX expert <img 
+      loading="lazy" 
+      src="https://emojipedia-us.s3.amazonaws.com/source/microsoft-teams/337/man-technologist-light-skin-tone_1f468-1f3fb-200d-1f4bb.png" data-src="https://emojipedia-us.s3.amazonaws.com/source/microsoft-teams/337/man-technologist-light-skin-tone_1f468-1f3fb-200d-1f4bb.png" 
+      data-srcset="https://emojipedia-us.s3.amazonaws.com/source/microsoft-teams/337/man-technologist-light-skin-tone_1f468-1f3fb-200d-1f4bb.png 2x" 
+      alt="Technologist" 
+      title="Technologist" 
+      width="72" 
+      height="72" 
+      srcset="https://emojipedia-us.s3.amazonaws.com/source/microsoft-teams/337/man-technologist-light-skin-tone_1f468-1f3fb-200d-1f4bb.png 2x"
+      />
+    </span>
+    <span in:typewriter on:introend="{() => underline = true}">with solid coding skills</span><span class="animate-pulse">|</span>
+  </h1>
+</section>
 
-<!-- Hi, I'm a UX expert 
-  <img 
-  loading="lazy" 
-  src="https://emojipedia-us.s3.amazonaws.com/source/microsoft-teams/337/man-technologist-light-skin-tone_1f468-1f3fb-200d-1f4bb.png" data-src="https://emojipedia-us.s3.amazonaws.com/source/microsoft-teams/337/man-technologist-light-skin-tone_1f468-1f3fb-200d-1f4bb.png" 
-  data-srcset="https://emojipedia-us.s3.amazonaws.com/source/microsoft-teams/337/man-technologist-light-skin-tone_1f468-1f3fb-200d-1f4bb.png 2x" 
-  alt="Man Technologist: Light Skin Tone" 
-  title="Man Technologist: Light Skin Tone" 
-  width="72" 
-  height="72" 
-  srcset="https://emojipedia-us.s3.amazonaws.com/source/microsoft-teams/337/man-technologist-light-skin-tone_1f468-1f3fb-200d-1f4bb.png 2x"
-  />
-  with solid coding skills| -->
-  
-  
+<style>
+  /* .underline {
+    @apply relative;
+    &:after {
+      content:
+    }
+  } */
+</style>
