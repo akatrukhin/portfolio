a0adeea2191a8503ea74cbcefcd400bb10bfd8b0
akatrukhin <a.katrukhin@gmail.com>
2022-08-31 17:10:01 +0000

    Deploying to gh-pages from  @ a0bb4f0020753551403045e31fff33cad85bb44f 🚀

---
+++ assets/About.53e36561.js
@@ -0,0 +1,2 @@
+import{S as o,i as r,s as i,e as c,a as u,n,d as l,o as p}from"./index.e714ce25.js";function f(s){let e;return{c(){e=c("h1"),e.innerHTML=`Hello
+  <span id="typed-string"></span>`},m(t,a){u(t,e,a)},p:n,i:n,o:n,d(t){t&&l(e)}}}function d(s){return p(()=>{}),[]}class h extends o{constructor(e){super(),r(this,e,d,f,i,{})}}export{h as default};
